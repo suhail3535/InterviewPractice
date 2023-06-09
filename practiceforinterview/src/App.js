42db3024ecb18f670aaa158b023e148d2e588e0b
suhail3535 <khansohail015@gmail.com>
2023-06-09 08:35:46 +0530

    debouncing and counter

--- practiceforinterview/src/App.js
+++ practiceforinterview/src/App.js
@@ -1,10 +1,12 @@
 
-import Ref2 from "./Component/Ref2";
+import Counter from "./Component/Counter";
+// import Ref2 from "./Component/Ref2";Counter
 
 function App() {
   return (
       <div className="App">
-<Ref2 />
+      {/* <Ref2 /> */}
+      <Counter />
       </div>
   );
 }
