ea74fd5a1147e50ee45f65f23d10ac5dbcfeb685
suhail3535 <khansohail015@gmail.com>
2023-06-19 13:21:44 +0530

    all curd operation

--- practiceforinterview/src/Component/PostData.jsx
+++ practiceforinterview/src/Component/PostData.jsx
@@ -2,64 +2,88 @@ import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 
 const PostData = () => {
-    const [user, setUser] = useState([])
+    const [user, setUser] = useState([]);
     const [name, setName] = useState("");
     const [age, setAge] = useState("");
     const [course, setCourse] = useState("");
+    const [updateId, setUpdateId] = useState(null);
 
     const newData = {
         name: name,
         age: age,
         course: course
     };
-
+//Post Request
     const postData = async () => {
         try {
             const response = await axios.post("http://localhost:8080/product", newData);
             console.log(response.data);
+            clearForm();
+            getData();
         } catch (error) {
             console.log(error);
         }
     };
+
     //get request
     const getData = async () => {
         try {
-            let userData = await axios.get("http://localhost:8080/product").then((res) => {
-                console.log("getuser", res.data)
-                setUser(res.data)
-            })
+            const response = await axios.get("http://localhost:8080/product");
+            console.log("getuser", response.data);
+            setUser(response.data);
         } catch (error) {
-            console.log(error)
+            console.log(error);
         }
-    }
-
-
-
-
+    };
+//Delet Request
     const deleteData = async (id) => {
         try {
             const response = await axios.delete(`http://localhost:8080/product/${id}`);
             console.log(response.data);
+            getData();
+        } catch (error) {
+            console.log(error);
+        }
+    };
+//Upadte Request
+    const updateData = async () => {
+        try {
+            const response = await axios.put(`http://localhost:8080/product/${updateId}`, newData);
+            console.log(response.data);
+            clearForm();
+            setUpdateId(null);
+            getData();
         } catch (error) {
             console.log(error);
         }
     };
 
-    const handleDelete = (id) => {
-        deleteData(id);
-    window.location.reload()
+    const handleUpdate = (id) => {
+        const selectedItem = user.find((item) => item.id === id);
+        setName(selectedItem.name);
+        setAge(selectedItem.age);
+        setCourse(selectedItem.course);
+        setUpdateId(id);
     };
-
-
-    useEffect(() => {
-        getData()
-    }, [])
-
+///most important 
     const handleSubmit = (e) => {
         e.preventDefault();
-        postData();
-        window.location.reload()
+        if (updateId) {
+            updateData();
+        } else {
+            postData();
+        }
     };
+
+    const clearForm = () => {
+        setName("");
+        setAge("");
+        setCourse("");
+    };
+
+    useEffect(() => {
+        getData();
+    }, []);
 
     return (
         <div>
@@ -71,22 +95,22 @@ const PostData = () => {
                 <input type="text" placeholder='age' value={age} onChange={(e) => setAge(e.target.value)} required /><br />
                 <label>Course:</label>
                 <input type="text" placeholder='course' value={course} onChange={(e) => setCourse(e.target.value)} required /><br />
-                <input type="submit" />
+                <input type="submit" value={updateId ? "Update" : "Submit"} />
             </form>
             <hr />
             <div style={{ display: "grid", gridTemplateColumns: "repeat(4,1fr)", gap: "20px" }}>
                 {user.map((item) => {
-                    return <div style={{ border: "1px dotted gray", padding: "10px", textAlign: "center" }}>
-                        <h1>{item.name}</h1>
-                        <p>{item.age}</p>
-                        <p>{item.course}</p>
-                        <button onClick={() => handleDelete(item.id)}>Delete</button>
-                    </div>
+                    return (
+                        <div key={item.id} style={{ border: "1px dotted gray", padding: "10px", textAlign: "center" }}>
+                            <h1>{item.name}</h1>
+                            <p>{item.age}</p>
+                            <p>{item.course}</p>
+                            <button onClick={() => handleUpdate(item.id)}>Edit</button>
+                            <button onClick={() => deleteData(item.id)}>Delete</button>
+                        </div>
+                    );
                 })}
             </div>
-
-
-
         </div>
     );
 };
